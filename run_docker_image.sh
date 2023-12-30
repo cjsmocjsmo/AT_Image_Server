15bcbfe6d240a720c81cad6b291997c16106cb54
Charlie_Smotherman <porthose.cjsmo.cjsmo@gmail.com>
2023-12-30 01:43:26 -0800

    Update Docker image version to 0.1.11

--- run_docker_image.sh
+++ run_docker_image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t ats_img_server:0.1.10 .;
-docker run -dit -p 9191:80 ats_img_server:0.1.10;
+docker build -t ats_img_server:0.1.11 .;
+docker run -dit -p 9191:80 ats_img_server:0.1.11;
