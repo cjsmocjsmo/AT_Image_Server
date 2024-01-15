05174c80440be88515cdc72ad3d6d43f2e1ff8fc
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-01-15 00:41:42 -0800

    add multiple main img sizes

--- images/old_images/landscape/webpconvert.sh
+++
@@ -1,3 +0,0 @@
-for f in /home/charliepi/Pictures/ats_images/landscape/*.jpg; do
-cwebp -q 95 -resize 1200 0 "$f" -o "${f%.jpg}.webp"
-done
--- images/old_images/portrait/webpconvert.sh
+++
@@ -1,3 +0,0 @@
-for f in /media/charliepi/ATree/AlphaTree/AT_Image_Server/images/portrait/*.jpg; do
-cwebp -q 95 -resize 200 0 "$f" -o "${f%.jpg}_thumb.webp"
-done
